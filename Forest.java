8aceddb0e96dcd1d11222a611474765722b2098e
unknown <sachinhl.xworkz@gmail.com>
2022-09-12 12:32:35 +0530

    forest file

---
+++ Forest.java
@@ -0,0 +1,18 @@
+class Forest {
+
+static String name="apple forest";
+
+static String typeofforest="green"; 
+
+static String region="india";
+
+static int totalarea=51249;
+
+static String [] primaryanimals={"dog", "fox", "deer","cobras", "snake","cheeta"};
+
+
+
+
+
+
+}
