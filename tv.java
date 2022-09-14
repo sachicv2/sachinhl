93086dd55204c959e017100dddbcf764446cdc6b
unknown <sachinhl.xworkz@gmail.com>
2022-09-14 10:50:13 +0530

    tv files

---
+++ tv.java
@@ -0,0 +1,10 @@
+class Tv
+{
+	static String brand = "onepluse";
+ 	 static int length=29;
+	 static float width=849f;
+	 static double barcoad=745478d;
+	 
+	  
+
+}
---
+++ tvRunner.java
@@ -0,0 +1,19 @@
+class tvRunner{
+ public static void main(String[] sachin)
+ 
+ { String brand=Tv.brand; //tv brand
+	 int length=Tv.length; // tv length
+	 float width=Tv.width;
+	 double barcoad=Tv.barcoad;
+	System.out.println("details for new brand Tv");
+   System.out.println("Brand;"+brand);
+   System.out.println("Length;"+length);
+   System.out.println("width;"+width);
+   System.out.println("barcoad;"+barcoad);
+ }
+ }
+
+ 
+ 
+ 
+ 
