f516c709d24442d359323a1faec8bf00b45357d2
unknown <sachinhl.xworkz@gmail.com>
2022-09-15 06:00:36 +0530

    mobile number

---
+++ MobileNumber.java
@@ -0,0 +1,84 @@
+class MobileNumber{
+	
+	static String getNameByMoblieNumber(long number)
+	 
+	 {
+		 System.out.println("Entered getNameByMoblieNumber")
+		 if (number==9902704864L)
+		 {
+			 System.out.println("sachin=9902704864");
+		 }
+		if (number==1234567892L)
+				{
+					System.out.println("vinay=1234567892");
+				}
+				 if (number==9902755864L)
+		 {
+			 System.out.println("R1=9902755864");
+		 }
+		if (number==1234594892L)
+				{
+					System.out.println("R2=1234594892");
+				}
+				 if (number==9902799864L)
+		 {
+			 System.out.println("R3=9902799864");
+		 }
+		if (number==12345679892L)
+				{
+					System.out.println("R4=12345679892");
+				}
+				 if (number==99027048641L)
+		 {
+			 System.out.println("R5=99027048641");
+		 }
+		if (number==12345678922L)
+				{
+					System.out.println("R6=12345678922");
+				}
+				if (number==99027048643L)
+		 {
+			 System.out.println("R7=99027048643");
+		 }
+		if (number==12345678924L)
+				{
+					System.out.println("R8=12345678924");
+				}
+				 if (number==99027048646L)
+		 {
+			 System.out.println("R9=99027048646");
+		 }
+		if (number==12345678927L)
+				{
+					System.out.println("R10=12345678927");
+				}
+				 if (number==99027048648L)
+		 {
+			 System.out.println("R11=99027048648");
+		 }
+		if (number==12345678992L)
+				{
+					System.out.println("R12=12345678992");
+				}
+				 if (number==99027048648L)
+		 {
+			 System.out.println("R13=99027048648");
+		 }
+		if (number==12345678929L)
+				{
+					System.out.println("R14=12345678929");
+				
+			}
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ MobileNumberRunner.java
@@ -0,0 +1,7 @@
+class MobileNumberRunner{
+	Public static void main(String[] arug)
+	{
+		String ref=MobileNumber.getNameByMoblieNumber(9902704864);
+		System.out.println(ref);
+	}
+}
